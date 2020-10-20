aaa8aa4420ec2a51178fe7c57f372126fd965c99
Alexander B <alex.batalov@workcall.com>
2020-10-20 21:27:08 +0300

    Added BigNumber.m file.

---
+++ ios/BigNumber.m
@@ -0,0 +1,13 @@
+//
+//  BigNumber.m
+//  zilpayMobile
+//
+//  Created by Alexander Batalov on 10/20/20.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(BigNumber, NSObject)
+RCT_EXTERN_METHOD(test:(NSString *)text)
+@end
